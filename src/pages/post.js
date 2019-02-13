22655c0d3d472051bf417db0480d5821fc58b383
Zachary Zulanas <zzulanas@gmail.com>
2019-02-13 14:33:52 -0800

    will netlify ever work?

--- src/pages/post.js
+++ src/pages/post.js
@@ -5,7 +5,6 @@ Used as a template for all blog posts
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
-import PageTransition from 'gatsby-plugin-page-transitions';
 
 export default function PostTemplate({ data }) {
   const { markdownRemark } = data // data.markdownRemark holds our post data
@@ -15,14 +14,12 @@ export default function PostTemplate({ data }) {
   };
   return (
     <Layout>
-      <PageTransition>
         <h1 className="title" style={blogStyle}>{frontmatter.title}</h1>
         <h2 className="subtitle">{frontmatter.date}</h2>
         <div
           className="content"
           dangerouslySetInnerHTML={{ __html: html }}
         />
-      </PageTransition>
     </Layout>
   )
 }
