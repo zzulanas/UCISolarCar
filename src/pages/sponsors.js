ff714d7f07eec92bba12197cb645a3079ffaee82
Zachary Zulanas <zzulanas@gmail.com>
2019-02-18 11:52:10 -0800

    I don't remember what I did but there's changes here

--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -13,40 +13,16 @@ const P = posed.p({
   exit: { x: 50, opacity: 0 },
 })
 
+
 const Sponsors = () => (
   <Layout>
     <SEO title="Sponsors" keywords={[`UCI`, `Solar`, `Car`]} />
     <Container>
       <div>
         <section className="section">
-          <div className="container">
-            <h1 className="title">Sponsors</h1>
-            <P>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed
-              diam in eros ultricies laoreet. Sed mattis ex et ante mattis, non
-              scelerisque est eleifend. Aliquam eget dolor aliquam urna
-              facilisis feugiat in eu orci. Pellentesque in mauris sed odio
-              congue finibus quis eget metus. Curabitur sed elit tellus. Etiam
-              sagittis arcu vel hendrerit porttitor. Sed id tellus venenatis,
-              auctor odio eget, consequat neque. Praesent eu nisi id dui mollis
-              auctor. Proin sollicitudin convallis facilisis. Suspendisse ut sem
-              turpis.
-            </P>
-          </div>
-        </section>
-        <section className="section">
-          <div className="container">
-            <P>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed
-              diam in eros ultricies laoreet. Sed mattis ex et ante mattis, non
-              scelerisque est eleifend. Aliquam eget dolor aliquam urna
-              facilisis feugiat in eu orci. Pellentesque in mauris sed odio
-              congue finibus quis eget metus. Curabitur sed elit tellus. Etiam
-              sagittis arcu vel hendrerit porttitor. Sed id tellus venenatis,
-              auctor odio eget, consequat neque. Praesent eu nisi id dui mollis
-              auctor. Proin sollicitudin convallis facilisis. Suspendisse ut sem
-              turpis.
-            </P>
+        <h1 className="title has-text-centered">Sponsors</h1>
+          <div className="container box">
+            
           </div>
         </section>
       </div>
