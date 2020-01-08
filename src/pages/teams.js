40145d455076d2e88fdc215baa60296e0908b08b
jessl22 <44043332+jessl22@users.noreply.github.com>
2020-01-07 20:08:41 -0800

    added pictures and new members

--- src/pages/teams.js
+++ src/pages/teams.js
@@ -13,10 +13,10 @@ const Teams = () => (
     <SEO title="Our Team" keywords={[`UCI`, `Solar`, `Car`]} />
     <section className="section">
       <div className="container">
+      <h1 className="title is-1 has-text-centered">Leadership</h1>
         <div className="columns">
           <div className="column" />
            <div className="column">
-            <h1 className="title is-1 has-text-centered">Leadership</h1>
             <TeamMember
               name="Subin Shrestha"
               description="Project Manager"
@@ -27,31 +27,105 @@ const Teams = () => (
           </div> 
           <div className="column" />
         </div>
-        <br></br>
-        <h1 className="title is-1 has-text-centered">Electrical</h1>
-        <div class="columns is-multiline is-mobile">
+        <div class="columns is-multiline is-mobile is-centered">
           <div class="column is-one-third">
             <TeamMember
               name="Gilberto Garcia"
               description="Electrical Lead"
               userlink="https://www.linkedin.com/in/gilberto-garcia-56aa7513a?trk=pub-pbmap"
               useremail="gilbermg@uci.edu"
-              imglink="https://tinyurl.com/yhvaxjbl"
+              imglink="https://tinyurl.com/yz9zsksk"
               blurb="Leg day is my favorite gym day."
             />
+          </div>
+          <div class="column is-one-third">
             <TeamMember
-              name="Jigar Hira"
-              description="Battery Lead"
-              userlink="https://www.linkedin.com/in/jigarhira"
-              useremail="hiraj@uci.edu"
-              imglink="https://tinyurl.com/yf2yo5wu"
+              name="Thomas Slagle"
+              description="Mechanical Lead"
+              userlink="https://www.linkedin.com/in/thomasslagle99/"
+              blurb="My beard is red but my hair isn't."
+              useremail="tslagle@uci.edu"
+              imglink="https://tinyurl.com/yjbzzn5f"
             />
+          </div>
+        </div>
+        <div class="columns is-multiline is-mobile">
+          <div class="column is-one-third">
             <TeamMember
               name="Keiser Ruiz"
               description="Telemetry Lead"
               userlink="https://www.linkedin.com/in/keiser-ruiz"
               useremail="ruizkf@uci.edu"
               imglink=""
+            />
+            <TeamMember
+              name="Benjamin Lam"
+              description="Battery Lead"
+              userlink="https://www.linkedin.com/in/benjamin-lam-70a80216b"
+              useremail="bxlam@uci.edu"
+              imglink=""
+            />
+            <TeamMember
+              name="Rahul Patel"
+              description="Suspension Lead"
+              userlink="https://www.linkedin.com/in/rpatel528/"
+              useremail="rahulup@uci.edu"
+              imglink="https://tinyurl.com/yeg67rjk"
+              blurb="I’ve never eaten a frozen pizza."
+            />
+          </div>
+          <div class="column is-one-third">
+            <TeamMember
+              name="Samin Riazi"
+              description="Solar Array Lead"
+              useremail="riazis@uci.edu"
+              userlink="https://www.linkedin.com/in/samin-riazi-abb780179"
+              imglink="https://tinyurl.com/ygycuanh"
+            />
+            <TeamMember
+              name="Brandon Chan"
+              description="Aerodynamics Lead"
+              imglink="https://tinyurl.com/yjw9qvny"
+              userlink="https://www.linkedin.com/in/brandonchan77"
+              useremail="chanbw1@uci.edu"
+              blurb="Water is in fact wet. Change my mind."
+            />
+          </div>
+          <div class="column is-one-third">
+            <TeamMember
+              name="Kevin Kuntjoro"
+              description="Motor Lead"
+              userlink="https://www.linkedin.com/in/kevinkuntjoro"
+              useremail="kkuntjor@uci.edu"
+              imglink=""
+            /> 
+            <TeamMember
+              name="Aaron Villanueva"
+              description="Human Interface Lead"
+              userlink="https://www.linkedin.com/in/manuel-aaron-villanueva-0346b1167"
+              useremail="villanm2@uci.edu"
+              blurb="I can improvise a jazz solo on the trumpet."
+              imglink="https://tinyurl.com/yk5eddgs"
+            />
+          </div>
+        </div>
+        <br></br>
+        <h1 className="title is-1 has-text-centered">Electrical</h1>
+        <div class="columns is-multiline is-mobile">
+          <div class="column is-one-third">
+            <TeamMember
+              name="Jigar Hira"
+              description="Battery Team"
+              userlink="https://www.linkedin.com/in/jigarhira"
+              useremail="hiraj@uci.edu"
+              imglink="https://tinyurl.com/yf2yo5wu"
+            />
+            <TeamMember
+              name="Nathan Trudeau"
+              description="Telemetry Team"
+              userlink="https://www.linkedin.com/in/nathan-trudeau-571863187/"
+              useremail="ntrudeau@uci.edu"
+              imglink="https://tinyurl.com/yg7d4q47"
             />
           </div>
           <div class="column is-one-third">
@@ -64,11 +138,12 @@ const Teams = () => (
               blurb="A biomedical engineering student interested in mechatronics, MEMS, sustainable energy, and machine learning. Enjoys playing guitar on his free time :)"
             />
             <TeamMember
-              name="Samin Riazi"
-              description="Solar Array Lead"
-              useremail="riazis@uci.edu"
-              userlink="https://www.linkedin.com/in/samin-riazi-abb780179"
-              imglink="https://tinyurl.com/ygycuanh"
+              name="Christian Mast"
+              description="Motor Team"
+              userlink="https://www.linkedin.com/in/mst1010"
+              useremail="cmast@uci.edu"
+              imglink="https://tinyurl.com/yz8f3u5u"
+              blurb="Catch me playing and watching soccer in my free time!"
             />
           </div>
           <div class="column is-one-third">
@@ -77,8 +152,7 @@ const Teams = () => (
               description="Solar Array Team"
               useremail="andreb1@uci.edu"
               userlink="http://www.linkedin.com/in/andreb1"
-              imglink=""
-              blurb="I have made 2 fiberglass reinforced wooden kayaks and a wood ribbed canvas canoe!"
+              imglink="https://tinyurl.com/yflaw7wo"
             />
             <TeamMember
               name="Kelly Lue"
@@ -95,14 +169,6 @@ const Teams = () => (
         <div class="columns is-multiline is-mobile">
           <div class="column is-one-third">
             <TeamMember
-              name="Thomas Slagle"
-              description="Mechanical Lead"
-              userlink="https://www.linkedin.com/in/thomasslagle99/"
-              blurb="My beard is red but my hair isn't."
-              useremail="tslagle@uci.edu"
-              imglink="https://tinyurl.com/yjbzzn5f"
-            />
-            <TeamMember
               name="Paulo Garcia"
               description="Manufacturing Team"
               useremail="paulocg@uci.edu"
@@ -110,52 +176,44 @@ const Teams = () => (
               imglink="https://tinyurl.com/yfwstxod"
             />
             <TeamMember
-              name="Brandon Chan"
-              description="Aerodynamics Lead"
-              imglink="https://tinyurl.com/yjw9qvny"
-              userlink="https://www.linkedin.com/in/brandonchan77"
-              useremail="chanbw1@uci.edu"
-              blurb="Water is in fact wet. Change my mind."
-            />
-          </div>
-          <div class="column is-one-third">
-            <TeamMember
               name="Drake Baldwin"
               description="Manufacturing Team"
               userlink="https://www.linkedin.com/in/drake-baldwin-20a739a6"
               useremail="ddbaldwi@uci.edu"
               imglink="https://tinyurl.com/yffuusad"
             />
+          </div>
+          <div class="column is-one-third">
+            <TeamMember
+              name="Nicole Alvarez"
+              description="Aerodynamics Team"
+              userlink="http://linkedin.com/in/nicole-martinne-alvarez"
+              useremail="nmalvar1@uci.edu"
+              imglink="https://tinyurl.com/yzn7omb4"
+              blurb="Volunteered in Marine Mammal Care Center for 7 months where I got to work with seals!"
+            />
+            <TeamMember
+              name="Edwin Christhuraj"
+              description="Aerodynamics Team"
+              userlink="https://www.linkedin.com/mwlite/me"
+              useremail="echristh@uci.edu"
+              imglink="https://tinyurl.com/yfbap952"
+            />
+          </div>
+          <div class="column is-one-third">
             <TeamMember
               name="Asem Alkhatib"
               description="Chassis Team"
               userlink="https://www.linkedin.com/in/mohammad-asem-alkhatib"
               useremail="malkhat1@uci.edu"
-            />
-          </div>
-          <div class="column is-one-third">
-            <TeamMember
-              name="Rahul Patel"
-              description="Suspension Lead"
-              userlink="https://www.linkedin.com/in/rpatel528/"
-              useremail="rahulup@uci.edu"
-              imglink="https://tinyurl.com/yeg67rjk"
-              blurb="I’ve never eaten a frozen pizza."
-            />
-            <TeamMember
-              name="Aaron Villanueva"
-              description="Human Interface Lead"
-              userlink="https://www.linkedin.com/in/manuel-aaron-villanueva-0346b1167"
-              useremail="villanm2@uci.edu"
-              blurb="I can improvise a jazz solo on the trumpet."
-              imglink="https://tinyurl.com/yk5eddgs"
+              imglink="https://tinyurl.com/yjfspf43"
             />
           </div>
         </div>
         <br></br>
         <h1 className="title is-1 has-text-centered">Operations</h1>
         <div class="columns is-multiline is-mobile">
-          <div className="column is-half">
+          <div className="column is-one-third">
             <TeamMember
               name="Colette Nguyen"
               description="Operations Team"
